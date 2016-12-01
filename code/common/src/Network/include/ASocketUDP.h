f5ab639a08fbdf41cacf14680a8cff78215c4439
Tookie26 <david.galy@epitech.eu>
2016-12-01 16:18:24 +0100

    add class ASocketUDP

---
+++ code/common/src/Network/include/ASocketUDP.h
@@ -0,0 +1,28 @@
+ /**
+ * @file ASocketUDP.hh
+ * @author Tookie
+ * @brief abstraction of UDP socket
+ */
+
+#pragma once
+
+#include "ASocket.hh"
+
+/**
+ * namespace that contains all network abstraction
+ */
+namespace network {
+
+    /**
+     * Representation of UDP socket abstraction
+     */
+    class ASocketUDP : public ASocket {
+    public:
+        /**
+         * Constructor of ASocketUDP
+         * @param port port use to socket connection
+         */
+        ASocketUDP(const unsigned short port);
+    };
+
+}
---
+++ code/common/src/Network/src/ASocketUDP.cpp
@@ -0,0 +1,15 @@
+/**
+ * @file ASocketUDP.cpp
+ * @author Tookie
+ * @brief abstraction of UDP socket
+ */
+
+#include "../include/ASocketUDP.hgit c"
+
+namespace network {
+
+    ASocketUDP::ASocketUDP(const unsigned short port) : ASocket(port) {
+
+    }
+
+}
