09070a4619a7c683b2130448f6a0805b4f6a1b17
Nekhot <pierre.rouss@epitech.eu>
2016-12-02 11:14:42 +0100

    remove testing file

--- code/common/fileSystemWatcher/src/AFileSystemWatcher.cpp
+++
@@ -1,6 +0,0 @@
-#include "AFileSystemWatcher.hh"
-
-AFileSystemWatcher::AFileSystemWatcher(const std::string &directoryName) : _directoryName(directoryName) {};
-AFileSystemWatcher::~AFileSystemWatcher(void) {};
-
-FileSystemWatcherException::FileSystemWatcherException(const std::string &e) : std::runtime_error(e) {};
