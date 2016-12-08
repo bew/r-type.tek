ec3a9aae1358a0c3fd86fa5c1b882aea5bb21062
Tookie26 <david.galy@epitech.eu>
2016-12-08 11:30:22 +0100

    implement class SockAddr

--- code/common/Network/src/SockAddr.cpp
+++ code/common/Network/src/SockAddr.cpp
@@ -1,5 +1,35 @@
-//
-// Created by Tookie on 08/12/2016.
-//
+/**
+ * @file SockAddr.cpp
+ * @author Tookie
+ * @brief encapsulation of sockaddr_in type
+ */
 
 #include "SockAddr.hh"
+
+namespace network {
+
+
+    SockAddr::SockAddr(unsigned short port, const std::string &addr)
+    {
+        _addr.sin_family = AF_INET;
+        _addr.sin_addr.s_addr = inet_addr(addr.c_str());
+        _addr.sin_port = htons(port);
+    }
+
+    SockAddr::SockAddr(unsigned short port)
+    {
+        _addr.sin_family = AF_INET;
+        _addr.sin_addr.s_addr = INADDR_ANY;
+        _addr.sin_port = htons(port);
+    }
+
+    sockaddr_in &SockAddr::getAddr()
+    {
+        return _addr;
+    }
+
+    void SockAddr::setAddr(const std::string &addr) {
+        _addr.sin_addr.s_addr = inet_addr(addr.c_str());
+    }
+
+}
