f57bc95758d447cb026e4de478fd8384b9ec3637
alexis <alexis.mongin@outlook.com>
2016-12-19 14:59:15 +0100

    Added hitbox component

---
+++ code/common/ECS/src/CompHitbox.cpp
@@ -0,0 +1,35 @@
+
+/**
+ * @file CompHitbox.cpp
+ * @author Alexis.
+ * @brief This component implement the hitboxes of game entities.
+ *
+ */
+
+#pragma once
+
+#include "CompHitbox.hh"
+
+namespace ECS {
+    namespace Component {
+
+	CompHitbox::CompHitbox(unsigned mid_width, unsigned mid_height)
+	    : AComponent(HITBOX), _midWidth(mid_width), _midHeight(mid_height)
+	{
+	}
+
+	CompHitbox::~CompHitbox()
+	{    
+	}
+
+	unsigned	CompHitbox::getMidWidth() const
+	{
+	    return _midHeight;
+	}
+
+	unsigned	CompHitbox::getMidHeight() const
+	{
+	    return _midWidth;
+	}
+    }
+}
