5a466d4fee49c57c4e24b6e9edb8a4e365471054
Christopher Paccard <christopher.paccard@epitech.eu>
2016-12-21 06:34:50 +0100

    Forgot the files

---
+++ code/common/Logs/src/DebugLogLevel.cpp
@@ -0,0 +1,29 @@
+/**
+ * @file DebugLogLevel.cpp
+ * @brief Debug log level class
+ * @author Christopher Paccard
+ *
+ * Debug log level class for Logger
+ *
+ */
+
+#include "DebugLogLevel.hh"
+
+namespace logs {
+    DebugLogLevel::DebugLogLevel() {
+        logger.registerLog(this);
+    }
+
+    DebugLogLevel::~DebugLogLevel() {
+    }
+
+    std::string DebugLogLevel::getLogLevelName() const {
+        return logs::DEBUG;
+    }
+
+    std::ostream &DebugLogLevel::getLogLevelStream() const {
+        return std::cout;
+    }
+
+    static DebugLogLevel debugLogLevel;
+}
