15f03d3298b8b50e44ee187a997eab384b0235d1
Tookie <david.galy@epitech.eu>
2016-12-21 14:28:34 +0100

    add comment for doxygen on server log class

--- code/server/include/ServerLogLevel.hh
+++ code/server/include/ServerLogLevel.hh
@@ -1,36 +1,69 @@
-//
-// Created by tookie on 12/21/16.
-//
+/**
+ * @file ServerLogLevel.hh
+ * @brief Server log level class
+ * @author Tookie
+ *
+ * Server log level class for Logger
+ *
+ */
 
 #pragma once
 
+#include "Logger.hh"
 #include <fstream>
 #include <string>
 #include <stdexcept>
 
+/**
+ * Contain logs stuff
+ */
 namespace logs
 {
-
+    /**
+     * Represent the name of the debug log level
+     */
     static const std::string SERVER = "server";
 
+    /**
+     * represents a server log, he's throw when log error occured
+     */
     using ServerLogException = std::runtime_error;
 
-    class ServerLogLevel: public ALogLevel
+    /**
+     * Server log level class for Logger
+     */
+    class ServerLogLevel : public ALogLevel
     {
     public:
+        /**
+         * constructor of ServerLogLevel
+         */
         ServerLogLevel();
 
+        /**
+         * Destructor
+         */
         virtual ~ServerLogLevel();
 
+        /**
+         * Get the name of the log level
+         *
+         * @return the name of the log level
+         */
         virtual std::string getLogLevelName() const;
 
-        virtual std::ostream& getLogLevelStream();
+        /**
+         * Get the stream of the log level to know where to write
+         *
+         * @return the stream of the log level to know where to write
+         */
+        virtual std::ostream &getLogLevelStream();
 
     private:
+        /**
+         * represents the file stream for log inside
+         */
         std::ofstream _file;
     };
 
 }
-
-
-#endif //PROJECT_SERVERLOGLEVEL_HPP
--- code/server/src/ServerLogLevel.cpp
+++ code/server/src/ServerLogLevel.cpp
@@ -1,7 +1,11 @@
-//
-// Created by tookie on 12/21/16.
-//
-
+/**
+ * @file ServerLogLevel.cpp
+ * @brief Server log level class
+ * @author Tookie
+ *
+ * Server log level class for Logger
+ *
+ */
 #include "ServerLogLevel.hh"
 
 namespace logs
@@ -11,7 +15,6 @@ namespace logs
         _file.open("server.log", std::ofstream::out | std::ofstream::app);
         if (!_file.is_open())
             throw ServerLogException("Could not open file server.log.");
-  //      logger.registerLogLevel(this);
     }
 
     ServerLogLevel::~ServerLogLevel()
