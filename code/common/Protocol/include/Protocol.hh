777f4591aa5730408229412e91f7670242655eb7
Christopher Paccard <christopher.paccard@epitech.eu>
2016-12-26 22:09:28 +0100

    Cleaner check protocol

--- code/common/Protocol/include/Protocol.hh
+++ code/common/Protocol/include/Protocol.hh
@@ -31,6 +31,11 @@ namespace protocol {
     const std::string version = "0.1";
 
     /**
+     * Allowed time in sec for a message to be interpreted (based on current and time +/- 1day).
+     */
+    const int64_t latency = 300;
+
+    /**
      * Get a valid header Document for the R-Type protocol
      *
      * @param action the action that will be put into the header Document
@@ -39,12 +44,44 @@ namespace protocol {
     bson::Document createHeader(const std::string &action);
 
     /**
+     * Check if the magic is right inside the given Document header
+     * 
+     * @param header the Document header to check
+     * @return true if the magic is right, else false
+     */
+    bool checkMagic(const bson::Document &header);
+
+    /**
+     * Check if the timestamp is right inside the given Document header
+     * 
+     * @param header the Document header to check
+     * @return true if the timestamp is right, else false
+     */
+    bool checkTimestamp(const bson::Document &header);
+
+    /**
+     * Check if the action is right inside the given Document header
+     * 
+     * @param header the Document header to check
+     * @return true if the action is right, else false
+     */
+    bool checkAction(const bson::Document &header);
+
+    /**
+     * Check if the version is right inside the given Document header
+     * 
+     * @param header the Document header to check
+     * @return true if the version is right, else false
+     */
+    bool checkVersion(const bson::Document &header);
+    
+    /**
      * Check if the given Document is a correct header message
      *
      * @param header the Document to check
      * @return true if the Document is correct, else false
      */
-    bool checkHeader(const bson::Document& header);
+    bool checkHeader(const bson::Document &header);
 
     /**
      * Check if the given Document is a correct message (depending on the action)
@@ -52,7 +89,7 @@ namespace protocol {
      * @param message the Document to check
      * @return true if the Document is correct, else false
      */
-    bool checkMessage(const bson::Document& message);
+    bool checkMessage(const bson::Document &message);
 }
 
 #endif //PROTOCOL_PROTOCOL_HH
--- code/common/Protocol/src/Protocol.cpp
+++ code/common/Protocol/src/Protocol.cpp
@@ -15,7 +15,8 @@ namespace protocol {
         bson::Document header;
 
         header << u8"magic" << protocol::magic;
-        header << u8"timestamp" << std::chrono::duration_cast<std::chrono::seconds>(std::chrono::high_resolution_clock::now().time_since_epoch()).count();
+        header << u8"timestamp" << std::chrono::duration_cast<std::chrono::seconds>(
+                std::chrono::high_resolution_clock::now().time_since_epoch()).count();
         header << u8"action" << action;
         header << u8"version" << std::string(protocol::version);
 
@@ -28,12 +29,39 @@ namespace protocol {
             {"Answer", &protocol::answers::checkAnswer},
     };
 
+    bool checkMagic(const bson::Document &header) {
+        return header.hasKey(u8"magic") &&
+               header[u8"magic"].getValueType() == bson::INT32 &&
+               header[u8"magic"].getValueInt32() == protocol::magic;
+    }
+
+    bool checkTimestamp(const bson::Document &header) {
+        int64_t now = std::chrono::duration_cast<std::chrono::seconds>(
+                std::chrono::high_resolution_clock::now().time_since_epoch()).count();
+        return header.hasKey(u8"timestamp") &&
+               header[u8"timestamp"].getValueType() == bson::INT64 &&
+               header[u8"timestamp"].getValueInt64() > now - (protocol::latency - 86400) &&
+               header[u8"timestamp"].getValueInt64() < now + (protocol::latency + 86400); // Yes, things can come from the future
+    }
+
+    bool checkAction(const bson::Document &header) {
+        return header.hasKey(u8"action") &&
+               header[u8"action"].getValueType() == bson::STRING &&
+               checkFunctions.count(header[u8"action"].getValueString());
+    }
+
+    bool checkVersion(const bson::Document &header) {
+        return header.hasKey(u8"version") &&
+               header[u8"version"].getValueType() == bson::STRING &&
+               header[u8"version"].getValueString() == protocol::version;
+    }
+
     bool checkHeader(const bson::Document &header) {
         return header.elementsCount() == 4 &&
-               header.hasKey(u8"magic") && header[u8"magic"].getValueType() == bson::INT32 && header[u8"magic"].getValueInt32() == protocol::magic &&
-               header.hasKey(u8"timestamp") && header[u8"timestamp"].getValueType() == bson::INT64 && header[u8"timestamp"].getValueInt64() > 0 &&
-               header.hasKey(u8"action") && header[u8"action"].getValueType() == bson::STRING && checkFunctions.count(header[u8"action"].getValueString()) &&
-               header.hasKey(u8"version") && header[u8"version"].getValueType() == bson::STRING && header[u8"version"].getValueString() == protocol::version;
+               protocol::checkMagic(header) &&
+               protocol::checkTimestamp(header) &&
+               protocol::checkAction(header) &&
+               protocol::checkVersion(header);
     }
 
     bool checkMessage(const bson::Document &message) {
