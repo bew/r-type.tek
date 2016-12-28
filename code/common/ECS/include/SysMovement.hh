8ded1a09e29dbda6d5fa3ed88df97f46b21a29ea
alexis <alexis.mongin@outlook.com>
2016-12-28 18:38:30 +0100

    limited player movement

--- code/common/ECS/include/SysMovement.hh
+++ code/common/ECS/include/SysMovement.hh
@@ -36,12 +36,7 @@ namespace ECS {
              */
             virtual ~SysMovement();
 
-            /**
-             * update function. Will update all CompMovement.
-             * @param data The persistent information of the ECS.
-             */
-            void        update(WorldData &data) override;
-
+            
             /**
              * Put an entity in the grid.
              * @param mov_comp movement component of the given entity.
@@ -56,6 +51,19 @@ namespace ECS {
              * @param component The movement component.
              */
             static void computeNextCoordinates(Component::CompMovement *component);
+
+            /**
+             * Check if the position is valid for a player entity.
+             * ie, check that the players don't leave the screen.
+             * @param player The entity. The function will check if this a player.
+             */
+            static void playerCheckPosition(Entity::Entity &player);
+
+            /**
+             * update function. Will update all CompMovement.
+             * @param data The persistent information of the ECS.
+             */
+            void        update(WorldData &data) override;
         };
     }
 }
