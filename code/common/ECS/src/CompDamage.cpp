3ea42b7942e23ff828f7eb2393d1cbd5d38e72c4
Christopher Paccard <christopher.paccard@epitech.eu>
2017-01-01 04:14:15 +0100

    CompDamage

--- code/common/ECS/src/CompDamage.cpp
+++ code/common/ECS/src/CompDamage.cpp
@@ -13,6 +13,16 @@ namespace ECS {
       damage(amount) {
     }
 
+      bson::Document  CompDamage::serialize() const {
+          bson::Document doc;
+          doc << u8"damage" << damage;
+          return doc;
+      };
+
+      void  CompDamage::deserialize(const bson::Document& document) {
+          document[u8"damage"] >> damage;
+      }
+
     const std::string &CompDamage::getType() const {
       return Component::DAMAGE;
     }
